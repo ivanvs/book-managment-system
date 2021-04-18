1e568b2fffaf94880048fa9df41f18454e9c6883
robince <robince885@gmail.com>
2021-04-18 19:17:50 +0200

    Save authors to mongo

--- src/book/dto/book.dto.ts
+++ src/book/dto/book.dto.ts
@@ -1,10 +1,22 @@
-import { IsNotEmpty, MaxLength } from 'class-validator';
+import {
+  ArrayMinSize,
+  IsArray,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+} from 'class-validator';
 export class BookDto {
   @IsNotEmpty()
+  @IsString()
   @MaxLength(256, { message: 'Title is too long' })
   readonly title: string;
 
   @IsNotEmpty()
+  @IsString()
   @MaxLength(2048, { message: 'Description is too long' })
   readonly description: string;
+
+  @IsArray()
+  @ArrayMinSize(1)
+  readonly authors: string[];
 }
